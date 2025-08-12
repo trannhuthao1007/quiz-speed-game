462f2019db97e1d3eac1910079d9a9dad3eadc10
Tran Nhu Thao <69711031+trannhuthao1007@users.noreply.github.com>
2025-08-12 14:25:16 +0700

    Update server.js

--- quiz-speed-render/server.js
+++ quiz-speed-render/server.js
@@ -76,4 +76,7 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=>console.log('Server listening', PORT));
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
